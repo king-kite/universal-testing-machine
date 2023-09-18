69b0a3c2fe6e1c312a1d0e5004d39614fd856946
kingkite <kitewebdevelopers@gmail.com>
2023-09-19 00:10:35 +0100

    Added the firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBmMhuNz5Ks0PBU3Y8QWmY9HETkSTAmBUY",
+  authDomain: "a-universal-testing-machine.firebaseapp.com",
+  databaseURL: "https://a-universal-testing-machine-default-rtdb.firebaseio.com",
+  projectId: "a-universal-testing-machine",
+  storageBucket: "a-universal-testing-machine.appspot.com",
+  messagingSenderId: "410984561066",
+  appId: "1:410984561066:web:feb487cbf37f43933ed3d3",
+  measurementId: "G-Y6QGVYVN2R"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
