986fc5d3f1d3ed53905bb477c94072491b908065
kingkite <kitewebdevelopers@gmail.com>
2023-10-09 09:12:42 +0100

    Added development mode for testing

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,16 +1,42 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
+// import { connectAuthEmulator, getAuth } from 'firebase/auth';
+import { connectDatabaseEmulator, getDatabase } from 'firebase/database';
+import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore';
+// import { connectStorageEmulator, getStorage } from 'firebase/storage';
 
-const firebaseConfig = {
-	apiKey: 'AIzaSyBmMhuNz5Ks0PBU3Y8QWmY9HETkSTAmBUY',
-	authDomain: 'a-universal-testing-machine.firebaseapp.com',
-	databaseURL: 'https://a-universal-testing-machine-default-rtdb.firebaseio.com',
-	projectId: 'a-universal-testing-machine',
-	storageBucket: 'a-universal-testing-machine.appspot.com',
-	messagingSenderId: '410984561066',
-	appId: '1:410984561066:web:feb487cbf37f43933ed3d3',
-	measurementId: 'G-Y6QGVYVN2R',
+const USE_FIREBASE_EMULATOR = import.meta.env.VITE_USE_FIREBASE_EMULATOR
+	? +import.meta.env.VITE_USE_FIREBASE_EMULATOR === 1
+	: false;
+
+// Your web app's Firebase configuration
+const config = {
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+	databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(config);
+
+// export const auth = getAuth(app);
+export const db = getDatabase(app);
+export const firestore = getFirestore(app);
+// export const storage = getStorage(app);
+
+if (USE_FIREBASE_EMULATOR) {
+	// const authUrl = `http://localhost:${
+	//   import.meta.env.VITE_FIREBASE_AUTH_EMULATOR_PORT
+	// }`;
+	const databasePort = import.meta.env.VITE_FIREBASE_DATABASE_EMULATOR_PORT;
+	const firestorePort = import.meta.env.VITE_FIREBASE_FIRESTORE_EMULATOR_PORT;
+	// const storagePort = import.meta.env.VITE_FIREBASE_STORAGE_EMULATOR_PORT;
+
+	// connectAuthEmulator(auth, authUrl, { disableWarnings: true });
+	connectDatabaseEmulator(db, 'localhost', +databasePort);
+	connectFirestoreEmulator(firestore, 'localhost', +firestorePort);
+	// connectStorageEmulator(storage, 'localhost', +storagePort);
+}
